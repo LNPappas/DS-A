9561cdec57d5c887e542dfb48eb5fd84b9daa94f
LNPappas <LPappas@gmail.com>
2019-08-09 16:26:27 -0700

    update

--- Practice.py
+++ Practice.py
@@ -1,6 +1,6 @@
 #brute force would be a nested loop restuling in O(a1*a2) or O(n*m)
 
-def same(a1, a2):
+def same(a1=[], a2=[]):
     '''
     boolean function determining if any given two arrays
     contain the same element. O(a1+a2) of O(n+m)
@@ -9,13 +9,18 @@ def same(a1, a2):
     for element in a1:
         test.update({str(element) : element})
     for e in a2:
-        if str(e) == test.get(str(e)):
+        if e == test.get(str(e)):
             return True
     return False
 
-a1 = ['a','b,','c']
-a2 = [1,2,3]
-a3 = [4,'z',2.5,2.0,'c']
+a1 = ['a','b,','c', None]
+a2 = [1,2,3,4,[]]
+a3 = [4,'z',2.5,2.0, None]
 print(same(a1,a2))
 print(same(a3,a2))
 print(same(a1,a3))
+print(same())
+print(same([None],a3))
+print(same(a3))
+print(same(a2))
+print(same(['c'],a1))
