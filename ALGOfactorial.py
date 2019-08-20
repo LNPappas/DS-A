1d5ab2137190aeb42c4f620402102754b19c793a
LNPappas <LPappas@gmail.com>
2019-08-20 14:32:26 -0700

    update

--- ALGOfactorial.py
+++ ALGOfactorial.py
@@ -1,41 +1,28 @@
 class Factorial():
 
     def __init__(self):
-        self.total = 0
+        pass
 
     def recursive(self, data=0):
-        if data == 0:
-            x = self.total
-            self.total = 0
-            return x
-
-        if self.total == 0:
-            self.total = data
-        else:
-            self.total = self.total*data
-
-        data -= 1
-        return self.recursive(data)
+        if data == 2:
+            return 2
+        return data * self.recursive(data-1)
 
     def iterative(self, info=0):
-
-        if self.total == 0:
-            self.total = info
-
-        while info > 1:
-            info -= 1
-            self.total = self.total*info
-
-        x = self.total
-        self.total = 0
-        return x
-        
-            
+        i = 2
+        answer = 1
+        if info == 2:
+            return 2
+        while i <= info:
+            answer = answer*i
+            i += 1
+        return answer       
 
         
 if __name__ == "__main__":
     f = Factorial()
     print(f.recursive(5))
     print(f.iterative(5))
+
 
 
