049fdf7faa173e9d98468437c28ea62db3a0091d
LNPappas <LPappas@gmail.com>
2020-04-16 15:42:43 -0700

    dynamic fib

--- Algorithms/ALGOrecursive_fibonacci.py
+++ Algorithms/ALGOrecursive_fibonacci.py
@@ -16,8 +16,19 @@ class Fibonacci():
             i += 1
         return a
 
+    def dynamic(self,n):
+        a,b = 1,1
+        for _ in range(n):
+            yield a
+            a,b = b,a+b
+        return result
 
 if __name__ == "__main__":
     f = Fibonacci()
-    print(f.recursive(5))
-    print(f.iterative(5))
+    print(f.recursive(6))
+    print(f.iterative(6))
+
+    result = []
+    for x in f.dynamic(6):
+        result.append(x)
+    print(result)
